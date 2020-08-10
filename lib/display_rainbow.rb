b1345e7707010bc636c9097dfa0a9fd64c17be48
Christerle Nounoute <christerlesn@gmail.com>
2020-08-10 21:13:40 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
-def display_rainbow (colors= "red")
-  puts "The colors of the rainbow is #{colors}"
+def display_rainbow (colors= ["red", "blue", "orange"])
+  puts "The colors of the rainbow is #{colors[2]}"
 end
 
 display_rainbow
